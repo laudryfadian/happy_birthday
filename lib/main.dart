2db0e957f2d3f9fc444288b0aa85e58327aaeaff
LAUDRY IZA FADIAN <skakmate87@gmail.com>
2021-08-23 10:27:25 +0700

    update warna background

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(scaffoldBackgroundColor: Colors.white),
       debugShowCheckedModeBanner: false,
       home: Birthday(),
     );
