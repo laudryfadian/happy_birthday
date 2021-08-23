fc59c9ae5d61b5baab11a9b64e27fb279ad952b3
LAUDRY IZA FADIAN <skakmate87@gmail.com>
2021-08-23 10:22:19 +0700

    happy birthdayyy

---
+++ android/app/src/main/kotlin/com/example/fia_birthday/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fia_birthday
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:floating_dots/floating_dots.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Birthday(),
+    );
+  }
+}
+
+class Birthday extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            alignment: Alignment.center,
+            child: Image.asset('assets/hbd.png'),
+          ),
+          FloatingDotGroup(
+            opacity: 0.5,
+            speed: DotSpeed.mixed,
+            size: DotSize.random,
+            trajectory: Trajectory.straight,
+            colors: [
+              Colors.redAccent,
+              Colors.yellowAccent,
+              Colors.greenAccent,
+              Colors.deepOrange,
+              Colors.purpleAccent,
+              Colors.lightGreenAccent
+            ],
+            number: 35,
+            direction: Direction.up,
+          )
+        ],
+      ),
+    );
+  }
+}
