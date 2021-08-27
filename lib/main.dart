0e405b2b5c231e5d3aa05eef85ea370e9098dda1
LAUDRY IZA FADIAN <skakmate87@gmail.com>
2021-08-27 13:42:49 +0700

    Completedd

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(scaffoldBackgroundColor: Colors.white),
       debugShowCheckedModeBanner: false,
-      home: Birthday(),
+      home: Gift(),
     );
   }
 }
@@ -42,6 +42,49 @@ class Birthday extends StatelessWidget {
           )
         ],
       ),
+    );
+  }
+}
+
+class Gift extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+          child: Center(
+              child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            padding: EdgeInsets.only(bottom: 10.0),
+            child: Text(
+              "Buka cepettt",
+              style: TextStyle(fontSize: 40.0),
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(builder: (context) => Birthday()),
+                (Route<dynamic> route) => false,
+              );
+            }, // Handle your callback.
+            splashColor: Colors.brown.withOpacity(0.5),
+            child: Ink(
+              height: 250,
+              width: 250,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/oke.gif'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          )
+        ],
+      ))),
     );
   }
 }
